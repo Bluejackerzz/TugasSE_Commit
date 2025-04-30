d2b1a7647eee31239cab7b672f06e6a96cd6318e
Christian Jodi <bluejackers514@gmail.com>
2025-04-30 14:12:07 +0700

    menambah fitur perkalian

--- testinggitse.cpp
+++ testinggitse.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 int main() {
-    int a, b, hasil;
+    int a, b, hasil,hasil2;
 
     cin >> a >> b;
 
     hasil = a + b;
+    hasil2 = a * b;
+
 
     cout << "Hasil Pertambahan" << hasil;
+    cout << "Hasil Perkalian" << hasil2;
+
     
     return 0;
 }
